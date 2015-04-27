7450b7736e4d1690bf793737395b6b63443aa26a
Zachary Yedidia <zyedidia@gmail.com>
2015-04-27 18:38:14 -0400

    Repl features

--- src/AnimatedPlots.jl
+++ src/AnimatedPlots.jl
@@ -10,4 +10,39 @@ include("graph.jl")
 include("axis.jl")
 include("plotwindow.jl")
 
+function static_plot(fun::Function, name="Plot", width=800, height=600)
+	window = create_window(name, width, height)
+	add_graph(window, Graph(fun))
+	redraw(window)
+	open_window(window)
+
+	return window
+end
+function static_plot(fun::Function, window::PlotWindow)
+	add_graph(window, Graph(fun))
+	redraw(window)
+end
+
+function create_window(name="Plot", width=800, height=600)
+	window = PlotWindow(name, width, height)
+	redraw(window)
+	return window
+end
+
+function open_window(window::PlotWindow)
+	@async begin
+		while isopen(window)
+			sleep(0)
+			check_input(window)
+			draw(window)
+		end
+		destroy(window.renderwindow)
+		window = nothing
+		println(window)
+	end
+	println("End")
+end
+
+export open_window, create_window, static_plot
+
 end
