202fd7979a23af39f89788b8532101ab708cf95e
Zachary Yedidia <zyedidia@gmail.com>
2015-05-04 10:22:02 -0400

    Fix log.jl

--- examples/log.jl
+++ examples/log.jl
@@ -1,10 +1,10 @@
 using AnimatedPlots
 
-log_graph = Graph(log, 3, SFML.magenta)
-exponential_graph = Graph(x -> 2^x, 3, SFML.cyan)
+log_graph = StaticGraph(log, color=SFML.magenta)
+exponential_graph = StaticGraph(x -> 2^x, color=SFML.cyan)
 exponential_graph.accuracy = 10
 
 plot(log_graph)
 plot(exponential_graph)
 
-waitfor(AnimatedPlots.windows[end])
+waitfor(current_window())
